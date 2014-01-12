48a98bbd784764114465b21e8cde0fcb38ccf349
Marcin Erdmann <erdi84@gmail.com>
2014-01-12 17:01:08 +0000

    Add java implementation for json and plaintext endpoints.

---
+++ ratpack/ratpack-java/src/main/java/ratpack/benchmarks/techempower/java/ResponseData.java
@@ -0,0 +1,7 @@
+package ratpack.benchmarks.techempower.java;
+
+public class ResponseData {
+  final static String SERVER_NAME = "Ratpack";
+  final static String MESSAGE_KEY = "message";
+  final static String MESSAGE_VALUE = "Hello, World!";
+}
---
+++ ratpack/setup_java.py
@@ -0,0 +1,23 @@
+import subprocess
+import sys
+import os
+import setup_util
+
+def start(args, logfile, errfile):
+  try:
+    subprocess.check_call("./gradlew clean ratpack-java:runInBackground", shell=True, cwd="ratpack", stderr=errfile, stdout=logfile)
+    return 0
+  except subprocess.CalledProcessError:
+    return 1
+
+def stop(logfile, errfile):
+  try:
+    p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    for line in out.splitlines():
+      if 'ratpack.launch.RatpackMain' in line:
+        pid = int(line.split(None, 2)[1])
+        os.kill(pid, 9)
+    return 0
+  except subprocess.CalledProcessError:
+    return 1
