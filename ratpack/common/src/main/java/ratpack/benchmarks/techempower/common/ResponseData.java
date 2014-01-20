ff1db75584dd2ac80a5a36c71eeb737610174fc2
Luke Daley <ld@ldaley.com>
2014-01-20 08:43:11 +0000

    Optimize the Java benchmark to be as close to Netty as possible.

--- ratpack/common/src/main/java/ratpack/benchmarks/techempower/common/ResponseData.java
+++ ratpack/common/src/main/java/ratpack/benchmarks/techempower/common/ResponseData.java
@@ -1,7 +1,18 @@
 package ratpack.benchmarks.techempower.common;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.util.CharsetUtil;
+
 public class ResponseData {
-  public final static String SERVER_NAME = "Ratpack";
+  public final static CharSequence TEXT_PLAIN = HttpHeaders.newEntity("text/plain");
+  public final static CharSequence APPLICATION_JSON = HttpHeaders.newEntity("application/json");
+  public final static CharSequence DATE = HttpHeaders.newEntity(HttpHeaders.Names.DATE);
+  public final static CharSequence SERVER = HttpHeaders.newEntity(HttpHeaders.Names.SERVER);
+  public final static CharSequence SERVER_NAME = HttpHeaders.newEntity("Ratpack");
+  public final static CharSequence CONTENT_TYPE = HttpHeaders.newEntity(HttpHeaders.Names.CONTENT_TYPE);
   public final static String MESSAGE_KEY = "message";
   public final static String MESSAGE_VALUE = "Hello, World!";
+  public final static ByteBuf MESSAGE_VALUE_BUFFER = Unpooled.directBuffer().writeBytes(MESSAGE_VALUE.getBytes(CharsetUtil.UTF_8));
 }
--- ratpack/ratpack-java/src/main/java/ratpack/benchmarks/techempower/java/HandlerFactory.java
+++ ratpack/ratpack-java/src/main/java/ratpack/benchmarks/techempower/java/HandlerFactory.java
@@ -1,13 +1,61 @@
 package ratpack.benchmarks.techempower.java;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.netty.handler.codec.http.HttpHeaders;
+import ratpack.handling.Context;
 import ratpack.handling.Handler;
+import ratpack.http.MutableHeaders;
+import ratpack.http.Response;
 import ratpack.launch.LaunchConfig;
 
-import static ratpack.guice.Guice.handler;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import static ratpack.benchmarks.techempower.common.ResponseData.*;
 
 public class HandlerFactory implements ratpack.launch.HandlerFactory {
 
+  private final ObjectMapper objectMapper = new ObjectMapper();
+  private final SimpleDateFormat format = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z");
+  private CharSequence date = HttpHeaders.newEntity(format.format(new Date()));
+
   public Handler create(LaunchConfig launchConfig) throws Exception {
-    return handler(launchConfig, new ModuleBootstrap(), new HandlerBootstrap());
+
+    launchConfig.getEventLoopGroup().scheduleWithFixedDelay(new Runnable() {
+      @Override
+      public void run() {
+        date = HttpHeaders.newEntity(format.format(new Date()));
+      }
+    }, 1000, 1000, TimeUnit.MILLISECONDS);
+
+    return new Handler() {
+      @Override
+      public void handle(Context context) throws Exception {
+        Response response = context.getResponse();
+        MutableHeaders headers = response.getHeaders();
+
+        headers.set(DATE, date);
+        headers.set(SERVER, SERVER_NAME);
+
+        String path = context.getRequest().getPath();
+
+        switch (path) {
+          case "plaintext":
+            headers.set(CONTENT_TYPE, TEXT_PLAIN);
+            response.send("text/plain", MESSAGE_VALUE_BUFFER.duplicate().retain());
+            break;
+          case "json":
+            headers.set(CONTENT_TYPE, APPLICATION_JSON);
+            byte[] bytes = objectMapper.writeValueAsBytes(Collections.singletonMap(MESSAGE_KEY, MESSAGE_VALUE));
+            response.send(bytes);
+            break;
+          default:
+            context.clientError(404);
+            break;
+        }
+      }
+    };
   }
 }
