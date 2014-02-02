fe02439c7b6fbd8dc16077c328903bb6077ac090
dimzava <dimzava@gmail.com>
2014-02-02 20:59:57 +0200

    Refactor specs using response clone everywhere.

--- ratpack/common-test/src/main/groovy/ratpack/benchmarks/techempower/test/TechempowerBenchmarkSpec.groovy
+++ ratpack/common-test/src/main/groovy/ratpack/benchmarks/techempower/test/TechempowerBenchmarkSpec.groovy
@@ -1,5 +1,6 @@
 package ratpack.benchmarks.techempower.test
 
+import com.jayway.restassured.builder.ResponseBuilder
 import com.jayway.restassured.response.Response
 import io.netty.handler.codec.http.HttpHeaders
 import ratpack.groovy.test.TestHttpClient
@@ -30,9 +31,10 @@ abstract class TechempowerBenchmarkSpec extends Specification {
     get("json")
 
     then:
-    def responseStr = response.asString()
-    responseStr == """{"${MESSAGE_KEY}":"${MESSAGE_VALUE}"}"""
-    assertResponseHeaders(response, 'application/json', responseStr)
+    def responseBody = response.asString()
+    responseBody == """{"${MESSAGE_KEY}":"${MESSAGE_VALUE}"}"""
+    def responseClone = cloneResponse(response, responseBody)
+    assertResponseHeaders(responseClone, 'application/json')
   }
 
   def "plaintext test type fulfills requirements"() {
@@ -40,16 +42,35 @@ abstract class TechempowerBenchmarkSpec extends Specification {
     get("plaintext")
 
     then:
-    def responseStr = response.asString()
-    responseStr == MESSAGE_VALUE
-    assertResponseHeaders(response, 'text/plain;charset=UTF-8', responseStr)
+    def responseBody = response.asString()
+    responseBody == MESSAGE_VALUE
+    def responseClone = cloneResponse(response, responseBody)
+    assertResponseHeaders(responseClone, 'text/plain;charset=UTF-8')
   }
 
-  void assertResponseHeaders(Response response, String expectedContentType, String responseText) {
+  void assertResponseHeaders(Response response, String expectedContentType) {
     assert response.contentType == expectedContentType
-    assert response.header(HttpHeaders.Names.CONTENT_LENGTH) == responseText.getBytes().length.toString()
+    assert response.header(HttpHeaders.Names.CONTENT_LENGTH) == response.asByteArray().length.toString()
     assert response.header(HttpHeaders.Names.SERVER) == SERVER_NAME.toString()
     def headerDate = new SimpleDateFormat(DATE_FORMAT).parse(response.header(HttpHeaders.Names.DATE))
     assert headerDate <= new Date()
+  }
+
+  /**
+   * Workaround to avoid {@code java.io.IOException: Attempted read on closed stream}.
+   * <p>
+   * Calling {@link com.jayway.restassured.internal.RestAssuredResponseImpl#asString()}
+   * after having called {@link com.jayway.restassured.internal.RestAssuredResponseImpl#jsonPath()}
+   * (or calling {@link com.jayway.restassured.internal.RestAssuredResponseImpl#asString()}
+   * or {@link com.jayway.restassured.internal.RestAssuredResponseImpl#asByteArray()} twice)
+   * will attempt a read on an {@code InputStream} which is already closed from the first call.
+   * <p>
+   * The workaround is to clone the response and issue the second call to the clone rather than
+   * to the original response object.
+   */
+  Response cloneResponse(Response response, String responseBody) {
+    final Response clone = new ResponseBuilder().clone(response).setBody(responseBody).build()
+    clone.hasExpectations = true
+    return clone
   }
 }
--- ratpack/ratpack-groovy/src/test/groovy/ratpack/benchmarks/techempower/groovy/TechempowerBenchmarksSpec.groovy
+++ ratpack/ratpack-groovy/src/test/groovy/ratpack/benchmarks/techempower/groovy/TechempowerBenchmarksSpec.groovy
@@ -1,7 +1,5 @@
 package ratpack.benchmarks.techempower.groovy
 
-import com.jayway.restassured.builder.ResponseBuilder
-import com.jayway.restassured.response.Response
 import groovy.sql.Sql
 import ratpack.benchmarks.techempower.common.World
 import ratpack.groovy.test.LocalScriptApplicationUnderTest
@@ -49,24 +47,6 @@ class TechempowerBenchmarksSpec extends ratpack.benchmarks.techempower.test.Tech
       getInt("id") >= 1 && getInt("id") <= DB_ROWS
       getInt("randomNumber") != null
     }
-    assertResponseHeaders(responseClone, 'application/json', responseBody)
-  }
-
-  /**
-   * Workaround to avoid {@code java.io.IOException: Attempted read on closed stream}.
-   * <p>
-   * Calling {@link com.jayway.restassured.internal.RestAssuredResponseImpl#asString()}
-   * after having called {@link com.jayway.restassured.internal.RestAssuredResponseImpl#jsonPath()}
-   * (or calling {@link com.jayway.restassured.internal.RestAssuredResponseImpl#asString()}
-   * or {@link com.jayway.restassured.internal.RestAssuredResponseImpl#asByteArray()} twice)
-   * will attempt a read on an {@code InputStream} which is already closed from the first call.
-   * <p>
-   * The workaround is to clone the response and issue the second call to the clone rather than
-   * to the original response object.
-   */
-  Response cloneResponse(Response response, String responseBody) {
-    final Response clone = new ResponseBuilder().clone(response).setBody(responseBody).build()
-    clone.hasExpectations = true
-    return clone
+    assertResponseHeaders(responseClone, 'application/json')
   }
 }
